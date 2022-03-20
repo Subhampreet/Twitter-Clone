4e25eff156b18710885f2d90e8804fbbd8b011c0
Subhampreet <subhampreet18@gmail.com>
2022-03-20 14:51:46 +0530

    Twitter Clone : Sidebar Component Update

---
+++ components/Sidebar.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Image from "next/image";
+import SidebarLink from "./SidebarLink";
+import {
+    HomeIcon,
+    HashtagIcon,
+    BellIcon,
+    InboxIcon,
+    BookmarkIcon,
+    ClipboardListIcon,
+    UserIcon,
+    DotsCircleHorizontalIcon,
+    DotsHorizontalIcon,
+} from "@heroicons/react/outline";
+
+function Sidebar() {
+  return (
+    <div className="hidden sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed h-full">
+      <div className="flex items-center justify-center w-14 h-14 hoverAnimation p-0 xl:ml-24">
+        <Image src="https://rb.gy/ogau5a" width={30} height={30} />
+      </div>
+
+      <div className="space-y-2.5 mt-4 mb-2.5 xl:ml-24">
+        <SidebarLink text="Home" Icon={HomeIcon} active />
+        <SidebarLink text="Explore" Icon={HashtagIcon} />
+        <SidebarLink text="Notifications" Icon={BellIcon} />
+        <SidebarLink text="Messages" Icon={InboxIcon} />
+        <SidebarLink text="Bookmarks" Icon={BookmarkIcon} />
+        <SidebarLink text="Lists" Icon={ClipboardListIcon} />
+        <SidebarLink text="Profile" Icon={UserIcon} />
+        <SidebarLink text="More" Icon={DotsCircleHorizontalIcon} />
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
---
+++ components/SidebarLink.js
@@ -0,0 +1,19 @@
+import { useRouter } from 'next/router'
+import React from 'react'
+
+function SidebarLink({Icon, text, active}) {
+const router = useRouter();
+  return (
+    <div
+      className={`text-[#d9d9d9] flex items-center justify-center xl:justify-start text-xl space-x-3 hoverAnimation ${
+        active && "font-bold"
+      }`}
+      onClick={() => active && router.push("/")}
+    >
+      <Icon className="h-7" />
+      <span className="hidden xl:inline">{text}</span>
+    </div>
+  )
+}
+
+export default SidebarLink
